673b2edc5e69132be46d9200a1e5685dcfcb8a9c
jodecry <2512057093@qq.com>
2020-06-18 15:23:23 +0800

    修改代码，实现添加

--- 文件/script.js
+++ 文件/script.js
@@ -1,39 +1,55 @@
 
-let task = []; //{title:"ddddd",done,false}
+let tasks = [];  //title:dddd,done:false
 
-function renderEditor(){
+function rendreEditor() {
     let inputEl = document.querySelector("#default-todo-panel .todo-editor > input");
-    // inputE1.onchange  = (e) =>{
-    //     console.log("text,",e.target.value);
+   
+    // inputEl.onchange = (e) =>{
+    //     console.log("text ", e.target.value);
+    //     console.log("input change: ", e);
     // };
 
-    let addEl = document.querySelector("#default-todo-panel .todo-editor > button");
-    addEl.onclick = (e) => {
-        console.log("add click");
-        let newTask = {
-            title: inputE1.value,
-            done:false,
-        };
-
-        inputE1.value  = "";
-
-        task.push(newTask);
-
-        console.log("task: ",tasks);
-
-        renderTaskItems();
+let addTask = () =>{
+    let newTask = {
+        title: inputEl.value,
+        done: false,
     };
+
+    inputEl.value = "";
+
+    tasks.push(newTask);
+    
+    console.log("tasks: ", tasks);
+    
+    renderTaskItems();
+};
+
+    inputEl.onkeypress = (e) =>{
+       
+        if (e.key=="Enter"){
+            addTask();
+        }
+    }
+
+    let addEl = document.querySelector("#default-todo-panel .todo-editor > button");
+    addEl.onclick = (e) =>{
+        addTask();
+       
+    };
+    
 }
 
-
-function renderTaskItems(){
+function renderTaskItems() {
     console.log("render items");
     let itemsEl = document.querySelector("#default-todo-panel .todo-items");
 
-    for(let i = 0; i < tasks.length; i++) {
+    itemsEl.querySelectorAll("div").forEach((node)=>node.remove());
+
+    console.log(itemsEl);
+   
+    for(let i = 0; i < tasks.length; i++ ) {
         let task = tasks[i];
         let itemEl = document.createElement("div");
-   
 
         let doneEl = document.createElement("input");
         doneEl.type = "checkbox";
@@ -47,11 +63,9 @@ function renderTaskItems(){
         cancelEl.innerText = "X";
         itemEl.append(cancelEl);
 
-        itemEl.append(itemEl);
-
+        itemsEl.append(itemEl);
     }
-
 }
 
-renderEditor();
+rendreEditor();
 renderTaskItems();
